6329ee12cef7996ac10f6cbad11b5b4a2fa8fc36
Marceli Kluzniak <marcelikluzniak5@gmail.com>
2021-06-10 19:30:35 +0200

    sample of overriding method

---
+++ src/pl/mkluzniak/Animal.java
@@ -0,0 +1,7 @@
+package pl.mkluzniak;
+
+public class Animal {
+    void speak() {
+        System.out.println("The animal speaks");
+    }
+}
---
+++ src/pl/mkluzniak/Dog.java
@@ -0,0 +1,8 @@
+package pl.mkluzniak;
+
+public class Dog {
+
+    void  speak(){ //overriding method from class animal
+        System.out.println("The dog goes bark");
+    }
+}
